5f0f94ccc596aec6685b0f32d629c8e5df9a7975
Bhavesh Wagh <bhaveshbm2711@gmail.com>
2024-06-22 14:20:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+  // Write your code here let result = [];
+    let current_chunk = [];
+    let current_sum = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (current_sum + arr[i] > n) {
+            result.push(current_chunk);
+            current_chunk = [arr[i]];
+            current_sum = arr[i];
+        } else {
+            current_chunk.push(arr[i]);
+            current_sum += arr[i];
+        }
+    }
+    
+    if (current_chunk.length > 0) {
+        result.push(current_chunk);
+    }
+    
+    return result;
 };
 
 const n = prompt("Enter n: ");
